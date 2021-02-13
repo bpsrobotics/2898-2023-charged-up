e6f4de01039406a31a1f3b69f11db9fc52b8d7fc
JuliaF <none@none.none>
2021-02-13 14:58:58 -0800

    Cleared up the process function.

--- src/main/kotlin/com/teamXXXX/robot/OI.kt
+++ src/main/kotlin/com/teamXXXX/robot/OI.kt
@@ -2,13 +2,19 @@ package com.teamXXXX.robot
 
 import edu.wpi.first.wpilibj.XboxController
 import kotlin.math.abs
+import kotlin.math.pow
 import kotlin.math.sign
-
+/**
+ * The Operator Interface object.
+ * This is where you put all of the joystick, button, or keyboard inputs.
+ */
 object OI {
+
     /**
-     * The Operator Interface file.
-     * This is where you put all of the joystick, button, or keyboard inputs.
+     * Threshold below which [process] will return 0.
+     * 0.1 historically used, but optimal value unknown.
      */
+    const val DEADBAND_THRESHOLD = 0.1
 
     /**
      * Utility function for controller axis, optional deadzone and square/cube for extra fine-grain control
@@ -20,7 +26,7 @@ object OI {
         var output = 0.0
 
         if (deadzone) {
-            output = if (abs(input) < 0.1) {
+            output = if (abs(input) < DEADBAND_THRESHOLD) {
                 0.0
             } else {
                 input
@@ -28,11 +34,13 @@ object OI {
         }
 
         if (square) {
-            output *= output * sign(output)
+            // To keep the signage for output, we multiply by sign(output). This keeps negative inputs resulting in negative outputs.
+            output = output.pow(2) * sign(output)
         }
 
         if (cube) {
-            output *= output * output
+            // Because cubing is an odd number of multiplications, we don't need to multiply by sign(output) here.
+            output = output.pow(3)
         }
 
         return output
