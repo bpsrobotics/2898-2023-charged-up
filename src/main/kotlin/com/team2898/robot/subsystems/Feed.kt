62de7dd42dda2c3ab599b5e5a67c39b3712f912f
Max Leibowitz <403089@bsd84.org>
2022-02-19 15:26:35 -0800

    Removed subsystem complex, moved functionality to feeder

--- src/main/kotlin/com/team2898/robot/subsystems/Feed.kt
+++ src/main/kotlin/com/team2898/robot/subsystems/Feed.kt
@@ -1,70 +1,95 @@
 package com.team2898.robot.subsystems
 
+import com.bpsrobotics.engine.utils.minus
+import com.bpsrobotics.engine.utils.seconds
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX
 import com.cuforge.libcu.Lasershark
-import com.team2898.robot.Constants
+import com.revrobotics.CANSparkMax
+import com.revrobotics.CANSparkMaxLowLevel.MotorType.kBrushless
+import com.team2898.robot.subsystems.Feed.State.*
+import edu.wpi.first.wpilibj.Timer
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 
+// TODO: merge with intake?  Otherwise add special state that the intake
+// puts it into to run the vectoring wheels while intaking the first ball
+
 object Feed : SubsystemBase() {
-    val ballDetector1 = Lasershark(Constants.FEEDER_BALL_DETECTOR_1)
-    val ballDetectorShooter = Lasershark(Constants.FEEDER_BALL_DETECTOR_SHOOTER)
-    val ballDetector2 = Lasershark(Constants.FEEDER_BALL_DETECTOR_2)
+    private val mainLasershark = Lasershark(1)  // TODO: port
 
-    // x and y are the CAN IDs of the sparks
-    private val topMotor = WPI_TalonSRX(Constants.FEEDER_TOP_MOTOR)
-    private val bottomMotor = WPI_TalonSRX(Constants.FEEDER_BOTTOM_MOTOR)
+    // unused at the moment
+    /* private val ballDetector1 = Lasershark(Constants.FEEDER_BALL_DETECTOR_1)
+    private val ballDetectorShooter = Lasershark(Constants.FEEDER_BALL_DETECTOR_SHOOTER)
+    private val ballDetector2 = Lasershark(Constants.FEEDER_BALL_DETECTOR_2) */
 
-    enum class Mode {
-        IDLE, FEED, SHOOT
-    }
+    private val vectorLeft = CANSparkMax(1, kBrushless)  // TODO: id
+    private val vectorRight = CANSparkMax(2, kBrushless)  // TODO: id
+    private val vector = MotorControllerGroup(vectorLeft, vectorRight)
 
-    var state = Mode.IDLE
+    private val feederMotor = WPI_TalonSRX(3) // TODO: id
 
-    private fun idleMotors() {
-        topMotor.set(0.0)
-        bottomMotor.set(0.0)
-    }
-
-    // we can determine the top motor's speed and the bottom motor's speed with some testing
-    private fun feedBall() {
-        if (ballDetector1.distanceCentimeters > 2.0 && ballDetector2.distanceCentimeters < 2.0) {
-            topMotor.set(Constants.TOP_SPEED)
-            bottomMotor.set(0.0)
-        } else {
-            topMotor.set(0.0)
-            bottomMotor.set(0.0)
-            state = Mode.IDLE
+    // public so that things can make decisions based on if it's ready or not
+    var state = EMPTY
+        private set(value) { // not settable from outside the object
+            if (value == SHOOTING) shootStartTime = Timer.getFPGATimestamp().seconds
+            field = value
         }
+
+    /** The reading range in which the feeder will run */
+    private val RUN_RANGE = 0.2..1.0
+
+    enum class State {
+        EMPTY, READY, FEEDING, SHOOTING
     }
 
-    fun shootBall() {
-        if (ballDetector1.distanceCentimeters < 2.0 && ballDetectorShooter.distanceCentimeters < 2.0) {
-            topMotor.set(Constants.TOP_SPEED)
-            bottomMotor.set(Constants.BOTTOM_SPEED)
-        } else {
-            topMotor.set(0.0)
-            bottomMotor.set(0.0)
-            state = Mode.IDLE
-        }
-    }
+    private var shootStartTime = 0.seconds
 
-    fun changeState(newState: Mode) {
-        if (newState != state) {
-            state = newState
-        }
-    }
+    // TODO: current limits
 
     override fun periodic() {
+        val distance = mainLasershark.distanceMeters
         when (state) {
-            Mode.IDLE -> {
-                idleMotors()
+            EMPTY -> {
+                vector.set(0.0)
+                feederMotor.set(0.0)
+
+                if (distance in RUN_RANGE) {
+                    state = FEEDING
+                }
             }
-            Mode.FEED -> {
-                feedBall()
+            FEEDING -> {
+                vector.set(1.0)  // TODO speed
+                feederMotor.set(1.0)
+
+                if (distance < RUN_RANGE.start) {
+                    state = READY
+                } else if (distance > RUN_RANGE.endInclusive) {
+                    state = EMPTY
+                }
             }
-            Mode.SHOOT -> {
-                shootBall()
+            READY -> {
+                vector.set(0.0)
+                feederMotor.set(0.0)
+
+                // has no automatic transition
             }
+            SHOOTING -> {
+                // TODO speeds
+                vector.set(1.0)
+                feederMotor.set(1.0)
+
+                // if it's been in the shooting state for long enough switch to empty
+                // will switch again if there's another ball
+                if ((Timer.getFPGATimestamp().seconds - shootStartTime).value > 0.5) {  // TODO: constant
+                    state = EMPTY
+                }
+            }
+        }
+    }
+
+    fun shoot() {
+        if (state == READY) {
+            state = SHOOTING
         }
     }
 }
