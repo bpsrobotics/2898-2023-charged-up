ed3eb4821f596b8d70e1f20c78b6c9caf1d12577
Wither <86643519+ItsDaWither@users.noreply.github.com>
2022-02-19 21:30:14 -0800

    Fix Shooter PID + Comment Test Code

--- src/main/kotlin/com/team2898/robot/subsystems/Shooter.kt
+++ src/main/kotlin/com/team2898/robot/subsystems/Shooter.kt
@@ -7,7 +7,6 @@ import com.revrobotics.CANSparkMax
 import com.revrobotics.CANSparkMax.ControlType
 import com.revrobotics.CANSparkMaxLowLevel
 import com.team2898.robot.Constants
-import com.team2898.robot.OI
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 import kotlin.math.abs
 import kotlin.math.absoluteValue
@@ -18,24 +17,16 @@ object Shooter : SubsystemBase() {
     private val shooterMotor = CANSparkMax(1, CANSparkMaxLowLevel.MotorType.kBrushless)
     private val spinnerMotor = CANSparkMax(2, CANSparkMaxLowLevel.MotorType.kBrushless)
     private var shooterGoal = 0.0
-    private val shooterActual by OI.Ramp.ramp(60.0) { shooterGoal }
     private var spinnerGoal = 0.0
-    private val spinnerActual by OI.Ramp.ramp(60.0) { spinnerGoal }
     private val shootCommand: Boolean get() = TODO() // TODO: Place every possible method of activating shooter here
 
     init {
-        // apply 5 volts if it's off by 15 rev/sec
-        val kP = (5.0 / 12) / (15 * 60)
-        val kD = kP / 5
+        // TODO Tune FF and PID loops, set constants
+        shooterMotor.pidController.p = 10.0
+        shooterMotor.pidController.ff = 2.5
 
-        listOf(shooterMotor, spinnerMotor).forEach {
-            it.restoreFactoryDefaults()
-            it.pidController.p = kP
-//            it.pidController.i = 0.000001
-            it.pidController.d = kD
-            it.pidController.ff = 0.00021
-//            it.pidController.setOutputRange(-0.3, 0.3)
-        }
+        spinnerMotor.pidController.p = 10.0
+        shooterMotor.pidController.ff = 2.0
     }
 
     enum class ShooterStates {
@@ -74,38 +65,28 @@ object Shooter : SubsystemBase() {
     override fun periodic() {
         val vel = max(shooterMotor.encoder.velocity.absoluteValue, spinnerMotor.encoder.velocity.absoluteValue)
         if (!isDisabled) {
-            shooterMotor.pidController.setReference(shooterActual, ControlType.kVelocity)
-            spinnerMotor.pidController.setReference(-spinnerActual, ControlType.kVelocity)
-            if (vel > (40.0 * 60)) {
-                println("RPM is $vel, over threshold, stopping")
-                disable()
-                return
-            } else if (max(shooterMotor.motorTemperature, spinnerMotor.motorTemperature) > 40.0) {
-                println(
-                    "Max motor temperature reached (${
-                        max(
-                            shooterMotor.motorTemperature,
-                            spinnerMotor.motorTemperature
-                        )
-                    }), stopping"
-                )
-                disable()
-                return
-            }
-            println(vel)
+            shooterMotor.pidController.setReference(shooterGoal, ControlType.kVelocity)
+            spinnerMotor.pidController.setReference(-spinnerGoal, ControlType.kVelocity)
+//            if (vel > (40.0 * 60)) {
+//                println("RPM is $vel, over threshold, stopping")
+//                disable()
+//                return
+//            } else if (max(shooterMotor.motorTemperature, spinnerMotor.motorTemperature) > 40.0) {
+//                println(
+//                    "Max motor temperature reached (${
+//                        max(
+//                            shooterMotor.motorTemperature,
+//                            spinnerMotor.motorTemperature
+//                        )
+//                    }), stopping"
+//                )
+//                disable()
+//                return
+//            }
+//            println(vel)
         } else {
-            shooterMotor.pidController.p = 0.0
-            shooterMotor.pidController.i = 0.0
-            shooterMotor.pidController.d = 0.0
-            shooterMotor.pidController.ff = 0.0
-
-            spinnerMotor.pidController.p = 0.0
-            spinnerMotor.pidController.i = 0.0
-            spinnerMotor.pidController.d = 0.0
-            spinnerMotor.pidController.ff = 0.0
-
-            shooterMotor.set(0.0)
-            spinnerMotor.set(0.0)
+            shooterMotor.stopMotor()
+            spinnerMotor.stopMotor()
         }
         when (state) {
             ShooterStates.IDLE -> if (shootCommand) {
