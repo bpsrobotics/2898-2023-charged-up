46a74e95c0cfc9042fae43723c8681f3fdc76516
Max Leibowitz <403089@bsd84.org>
2022-02-21 12:16:36 -0800

    changes!

--- src/main/kotlin/com/team2898/robot/subsystems/Shooter.kt
+++ src/main/kotlin/com/team2898/robot/subsystems/Shooter.kt
@@ -7,10 +7,10 @@ import com.revrobotics.CANSparkMax
 import com.revrobotics.CANSparkMax.ControlType
 import com.revrobotics.CANSparkMaxLowLevel
 import com.team2898.robot.Constants
-import com.team2898.robot.Constants.EJECT_SPEED
 import edu.wpi.first.wpilibj.Timer
 import edu.wpi.first.wpilibj2.command.SubsystemBase
-import kotlin.math.abs
+import kotlin.math.absoluteValue
+import kotlin.math.max
 
 object Shooter : SubsystemBase() {
     // TODO: constants
@@ -18,8 +18,6 @@ object Shooter : SubsystemBase() {
     private val spinnerMotor = CANSparkMax(2, CANSparkMaxLowLevel.MotorType.kBrushless)
     private var shooterGoal = 0.0
     private var spinnerGoal = 0.0
-    private var spinUpCommand = false
-    private var spinUpDumpCommand = false
     private var lastShotTime = 0.0
 
     init {
@@ -31,50 +29,41 @@ object Shooter : SubsystemBase() {
         shooterMotor.pidController.ff = 2.0
     }
 
-    fun shoot(enabled: Boolean){
-        spinUpCommand = enabled
+    fun shoot() {
+        lastShotTime = Timer.getFPGATimestamp()
+        state = ShooterStates.SPINUP
+        val speeds = Interpolation.getRPMs()
+        setGoals(speeds.first, speeds.second)
     }
-    fun dump(enabled: Boolean){
-        spinUpDumpCommand = enabled
+
+    fun dump() {
+        lastShotTime = Timer.getFPGATimestamp()
+        state = ShooterStates.SPINUP
+        setGoals(Constants.EJECT_SPEED.first, Constants.EJECT_SPEED.second)
     }
 
 
     enum class ShooterStates {
         IDLE,
-        ENABLED,
-        READY,
-        DUMP_ENABLED,
-        DUMP_READY
+        SPINUP,
+        READY
     }
 
     var state = ShooterStates.IDLE
         private set
 
-    fun setRPM(shooterSpeed: RPM, spinnerSpeed: RPM) {
-        if (isDisabled) return
+    private fun setGoals(shooterSpeed: RPM, spinnerSpeed: RPM) {
         shooterGoal = shooterSpeed.value
         spinnerGoal = spinnerSpeed.value
-//        controllerA.pidController.setReference(speed, ControlType.kVelocity)
-//        controllerB.pidController.setReference(-speed, ControlType.kVelocity)
     }
 
     fun getRPM(): Pair<RPM, RPM> {
         return Pair(RPM(shooterMotor.encoder.velocity), RPM(spinnerMotor.encoder.velocity))
     }
 
-    private var isDisabled = false
-
-    fun disable() {
-        isDisabled = true
-    }
-
-    fun enable() {
-        isDisabled = false
-    }
-
     override fun periodic() {
 //        val vel = max(shooterMotor.encoder.velocity.absoluteValue, spinnerMotor.encoder.velocity.absoluteValue)
-        if (!isDisabled) {
+        if (state != ShooterStates.IDLE) {
             shooterMotor.pidController.setReference(shooterGoal, ControlType.kVelocity)
             spinnerMotor.pidController.setReference(-spinnerGoal, ControlType.kVelocity)
 //            if (vel > (40.0 * 60)) {
@@ -98,55 +87,31 @@ object Shooter : SubsystemBase() {
             shooterMotor.stopMotor()
             spinnerMotor.stopMotor()
         }
+
         when (state) {
-            ShooterStates.IDLE -> {
-                if (spinUpCommand) {
-                    state = ShooterStates.ENABLED
-                }
-                if (spinUpDumpCommand) {
-                    state = ShooterStates.DUMP_ENABLED
-                }
-            }
-            ShooterStates.ENABLED -> {
-                val targetMotorSpeeds = Interpolation.getRPMs()
-                setRPM(targetMotorSpeeds.first, targetMotorSpeeds.second)
-                if (abs((getRPM().first - targetMotorSpeeds.first).value) < Constants.SHOOTER_THRESHOLD && abs((getRPM().second - targetMotorSpeeds.second).value) < Constants.SHOOTER_THRESHOLD) {
+            ShooterStates.IDLE -> {}
+            ShooterStates.SPINUP -> {
+                val shooterDiff = getRPM().first - shooterGoal.RPM
+                // TODO: sign
+                val spinnerDiff = getRPM().second - spinnerGoal.RPM
+                if (max(spinnerDiff.value.absoluteValue, shooterDiff.value.absoluteValue) < Constants.SHOOTER_THRESHOLD) {
                     state = ShooterStates.READY
                 }
-                if (lastShotTime < Timer.getFPGATimestamp() - 5.0){
+                if (lastShotTime < Timer.getFPGATimestamp() - 5.0) {
                     state = ShooterStates.IDLE
                 }
             }
             ShooterStates.READY -> {
-                val targetMotorSpeeds = Interpolation.getRPMs()
-                setRPM(targetMotorSpeeds.first, targetMotorSpeeds.second)
-                if (abs((getRPM().first - targetMotorSpeeds.first).value) > Constants.SHOOTER_THRESHOLD || abs((getRPM().second - targetMotorSpeeds.second).value) > Constants.SHOOTER_THRESHOLD) {
-                    state = ShooterStates.ENABLED
+                val shooterDiff = getRPM().first - shooterGoal.RPM
+                // TODO: sign
+                val spinnerDiff = getRPM().second - spinnerGoal.RPM
+                if (max(spinnerDiff.value.absoluteValue, shooterDiff.value.absoluteValue) > Constants.SHOOTER_THRESHOLD) {
+                    state = ShooterStates.SPINUP
                 }
-                if (lastShotTime < Timer.getFPGATimestamp() - 5.0){
+                if (lastShotTime < Timer.getFPGATimestamp() - 5.0) {
                     state = ShooterStates.IDLE
                 }
             }
-            ShooterStates.DUMP_ENABLED -> {
-                setRPM(EJECT_SPEED.first, EJECT_SPEED.second)
-                if (abs((getRPM().first - EJECT_SPEED.first).value) < Constants.SHOOTER_THRESHOLD && abs((getRPM().second - EJECT_SPEED.second).value) < Constants.SHOOTER_THRESHOLD) {
-                    state = ShooterStates.READY
-                }
-                if (lastShotTime < Timer.getFPGATimestamp() - 5.0){
-                    state = ShooterStates.IDLE
-                }
-            }
-            ShooterStates.DUMP_READY -> {
-                setRPM(EJECT_SPEED.first, EJECT_SPEED.second)
-                if (abs((getRPM().first - EJECT_SPEED.first).value) > Constants.SHOOTER_THRESHOLD || abs((getRPM().second - EJECT_SPEED.second).value) > Constants.SHOOTER_THRESHOLD) {
-                    state = ShooterStates.ENABLED
-                }
-                if (lastShotTime < Timer.getFPGATimestamp() - 5.0){
-                    state = ShooterStates.IDLE
-                }
-            }        }
-        if (spinUpCommand || spinUpDumpCommand){
-            lastShotTime = Timer.getFPGATimestamp()
         }
     }
 }
