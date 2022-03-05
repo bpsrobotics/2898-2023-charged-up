900923229aea0c06f247165634ee8ce75a95c34c
Wither <86643519+ItsDaWither@users.noreply.github.com>
2022-03-04 17:36:54 -0800

    Hide Auto Dump Preload

--- src/main/kotlin/com/team2898/robot/commands/HideAuto.kt
+++ src/main/kotlin/com/team2898/robot/commands/HideAuto.kt
@@ -3,11 +3,13 @@ package com.team2898.robot.commands
 import com.bpsrobotics.engine.utils.TrajectoryUtils.invertTrajectory
 import com.bpsrobotics.engine.utils.`M/s`
 import com.pathplanner.lib.PathPlanner
+import com.team2898.robot.commands.auto.FireLowBall
 import com.team2898.robot.commands.auto.FollowPath
 import com.team2898.robot.subsystems.Drivetrain
 import com.team2898.robot.subsystems.Odometry
 import edu.wpi.first.math.trajectory.Trajectory
 import edu.wpi.first.wpilibj.DriverStation
+import edu.wpi.first.wpilibj.Timer
 import edu.wpi.first.wpilibj.smartdashboard.Field2d
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
 import edu.wpi.first.wpilibj2.command.Command
@@ -16,6 +18,7 @@ import edu.wpi.first.wpilibj2.command.CommandBase
 class HideAuto : CommandBase() {
     lateinit var moveCommandGroup: Command
     private val field = Field2d()
+    val timer = Timer()
 
     override fun initialize() {
         var firstPath: Trajectory = PathPlanner.loadPath("HideAuto", 8.0, 1.5, true) // TODO: Max Viable Speed
@@ -28,16 +31,20 @@ class HideAuto : CommandBase() {
         field.robotPose = firstPath.initialPose
         SmartDashboard.putData(field)
 
-//        moveCommandGroup = FollowPath(firstPath, true)
-//
-//        moveCommandGroup.schedule()
+        moveCommandGroup = FireLowBall(1)
+
+        moveCommandGroup.schedule()
+
+        timer.reset()
     }
 
     override fun execute() {
         field.robotPose = Odometry.pose
         SmartDashboard.putData(field)
 
-        Drivetrain.stupidDrive(`M/s`(-0.25), `M/s`(-0.25))
+        if (timer.hasElapsed(2.0)) {
+            Drivetrain.stupidDrive(`M/s`(-0.2), `M/s`(-0.2))
+        }
     }
 
     override fun isFinished(): Boolean {
@@ -45,6 +52,6 @@ class HideAuto : CommandBase() {
     }
 
     override fun end(interrupted: Boolean) {
-//        moveCommandGroup.end(interrupted)
+        moveCommandGroup.end(interrupted)
     }
 }
