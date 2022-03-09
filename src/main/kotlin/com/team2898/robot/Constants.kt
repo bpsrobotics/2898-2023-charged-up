ee64d1fcbcfc7abd6390dfd95d562f16ff107e11
Max Leibowitz <403089@bsd84.org>
2022-03-08 18:55:30 -0800

    feeder distance

--- src/main/kotlin/com/team2898/robot/Constants.kt
+++ src/main/kotlin/com/team2898/robot/Constants.kt
@@ -52,7 +52,7 @@ object Constants {
     // Distance at which ball is in shooter
     const val FEEDER_MIN_DISTANCE = 0.4
     // Primary LaserShark distance threshold that corresponds to no ball in feeder
-    const val FEEDER_MAX_DISTANCE = 0.65
+    const val FEEDER_MAX_DISTANCE = 0.6
 
     // Control Scheme Constants
     enum class DriverMap {
