9730eed0575445e71ba4fc96b8021c59d785cac2
Max Leibowitz <403089@bsd84.org>
2022-03-17 13:40:14 -0700

    Maybe fix vision networktable reading

--- src/main/kotlin/com/team2898/robot/subsystems/Vision.kt
+++ src/main/kotlin/com/team2898/robot/subsystems/Vision.kt
@@ -2,7 +2,7 @@ package com.team2898.robot.subsystems
 
 import com.bpsrobotics.engine.utils.m
 import com.bpsrobotics.engine.utils.rad
-import com.bpsrobotics.engine.utils.seconds
+import edu.wpi.first.networktables.EntryListenerFlags
 import edu.wpi.first.networktables.NetworkTableInstance
 import edu.wpi.first.wpilibj.Timer
 import edu.wpi.first.wpilibj2.command.SubsystemBase
@@ -12,35 +12,27 @@ object Vision : SubsystemBase() {
     private val distanceEntry = table.getEntry("distance")
     private val angleEntry = table.getEntry("angle")
 
-    // private so they can be used in a threadsafe manner
-    private var internalDistance = 0.m
-    private var internalAngle = 0.rad
+    // who knows what thread the listeners fire on
+    private val lock = Any()
 
-    // TODO: questionably thread safe
     val lastUpdated = Timer()
+        get() = synchronized(lock) { field }
+
+    // volatile = not cached, to avoid potential thread safety issues
+    @Volatile
     var distance = 0.m
+        private set  // only settable internally
+
+    @Volatile
     var angle = 0.rad
+        private set
 
     init {
         lastUpdated.start()
         distanceEntry.addListener({
-            // every time the vision values are updated, copy them to the class fields
-            // synchronized for thread safety
-            synchronized(Vision) {
-                lastUpdated.reset()
-                internalDistance = it.value.double.m
-                internalAngle = angleEntry.value.double.rad
-            }
-        }, 0)
-    }
-
-    override fun periodic() {
-        // copy the grabbed values to the public fields
-        synchronized(Vision) {
-            distance = internalDistance
-            angle = internalAngle
-        }
-        distance = distanceEntry.getDouble(0.0).m
-        angle = angleEntry.getDouble(0.0).rad
+            synchronized(lock) { lastUpdated.reset() }
+            distance = it.value.double.m
+            angle = angleEntry.value.double.rad
+        }, EntryListenerFlags.kUpdate)
     }
 }
