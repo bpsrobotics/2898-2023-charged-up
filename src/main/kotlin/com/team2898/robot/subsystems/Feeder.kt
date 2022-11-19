22765191f827e36b422d32a49bd319a2f9235376
Margalit <nope@nope.nope>
2022-11-19 14:27:57 -0800

    simplified feeder controls

--- src/main/kotlin/com/team2898/robot/subsystems/Feeder.kt
+++ src/main/kotlin/com/team2898/robot/subsystems/Feeder.kt
@@ -3,6 +3,8 @@ package com.team2898.robot.subsystems
 import com.ctre.phoenix.motorcontrol.TalonSRXControlMode
 import com.ctre.phoenix.motorcontrol.can.TalonSRX
 import edu.wpi.first.wpilibj.DoubleSolenoid
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value.*
 import edu.wpi.first.wpilibj.PneumaticsModuleType
 import edu.wpi.first.wpilibj.Timer
 import edu.wpi.first.wpilibj2.command.SubsystemBase
@@ -12,36 +14,31 @@ object Feeder : SubsystemBase() {
     private val gateSolenoid = DoubleSolenoid(PneumaticsModuleType.CTREPCM, 0, 1 )
 
     private var state = FeederState.STOPPED
-    enum class FeederState {
-        STOPPED, INTAKING, OUTTAKING
+    enum class FeederState(val solenoid: Value, val motor: Double) {
+        STOPPED(kForward, 0.0),
+        INTAKING(kForward, 1.0),
+        OUTTAKING(kReverse, 1.0)
     }
-
     var lastSwitchTime = 0.0
 
     override fun periodic() {
+        feederMotor.set(TalonSRXControlMode.PercentOutput, state.motor)
+        gateSolenoid.set(state.solenoid)
         when (state) {
-            FeederState.STOPPED -> {
-                feederMotor.set(TalonSRXControlMode.PercentOutput, 0.0)
-                gateSolenoid.set(DoubleSolenoid.Value.kForward)
-            }
+            FeederState.STOPPED -> {}
             FeederState.INTAKING -> {
-                feederMotor.set(TalonSRXControlMode.PercentOutput, 1.0)
-                gateSolenoid.set(DoubleSolenoid.Value.kForward)
-
                 if (Timer.getFPGATimestamp() - lastSwitchTime > 1.0) {
                     state = FeederState.STOPPED
                 }
             }
             FeederState.OUTTAKING -> {
-                feederMotor.set(TalonSRXControlMode.PercentOutput, 1.0)
-                gateSolenoid.set(DoubleSolenoid.Value.kReverse)
-
                 if (Timer.getFPGATimestamp() - lastSwitchTime > 1.0) {
                     state = FeederState.STOPPED
                 }
             }
         }
     }
+
     fun startIntaking(){
         if(state == FeederState.OUTTAKING) {
             return
