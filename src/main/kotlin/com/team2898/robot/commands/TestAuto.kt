ed40545b2ba14bee9b68715e643ce2f509dce996
Ozy King <ozypfl@gmail.com>
2022-12-08 16:21:32 -0800

    Loads path file into trajectory

--- src/main/kotlin/com/team2898/robot/commands/TestAuto.kt
+++ src/main/kotlin/com/team2898/robot/commands/TestAuto.kt
@@ -2,14 +2,23 @@ package com.team2898.robot.commands
 
 import com.bpsrobotics.engine.utils.Degrees
 import com.bpsrobotics.engine.utils.M
+import com.team2898.robot.Constants
+import com.pathplanner.lib.PathPlanner
 import com.team2898.robot.subsystems.Drivetrain
 import com.team2898.robot.subsystems.Odometry
 import edu.wpi.first.math.geometry.Pose2d
 import edu.wpi.first.math.geometry.Rotation2d
+import edu.wpi.first.math.trajectory.Trajectory
 import edu.wpi.first.wpilibj2.command.CommandBase
 
 class TestAuto : CommandBase() {
-    private val path = Drivetrain.trajectoryMaker.builder()
+    // Steals path file and makes trajectory
+    private var firstPath: Trajectory = PathPlanner.loadPath(
+            "bunnybot-initial-path.path",
+            Constants.DRIVETRAIN_MAX_VELOCITY.metersPerSecondValue(),
+            Constants.DRIVETRAIN_MAX_ACCELERATION.metersPerSecondSquaredValue()
+    )
+    /*private val path = Drivetrain.trajectoryMaker.builder()
         .start(Pose2d())
         .point(1.5, 1.5)
         .point(0.0, 3.0)
@@ -19,11 +28,11 @@ class TestAuto : CommandBase() {
         .point(0.0, 3.0)
         .point(-1.5, 1.5)
         .end(Pose2d(0.0, 0.0, Rotation2d.fromDegrees(0.0)))
-        .build()
+        .build()*/
 
     override fun initialize() {
         Odometry.reset(M(0.0), M(0.0), Degrees(0.0))
-        Drivetrain.follow(path)
+        Drivetrain.follow(firstPath)
     }
 
     override fun isFinished(): Boolean {
