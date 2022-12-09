5516b02efe6453b734b81b652137df6b9ec3e059
Ori <egg@gmail.com>
2022-12-08 17:01:20 -0800

    Added Auto code

---
+++ src/main/kotlin/com/team2898/robot/commands/RobotAuto.kt
@@ -0,0 +1,27 @@
+package com.team2898.robot.commands
+
+import edu.wpi.first.wpilibj.smartdashboard.Field2d
+import edu.wpi.first.wpilibj2.command.Command
+import edu.wpi.first.wpilibj2.command.CommandBase
+import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup
+import edu.wpi.first.wpilibj2.command.WaitCommand
+
+class RobotAuto : CommandBase() {
+    lateinit var moveCommandGroup: Command
+    override fun initialize() {
+
+        moveCommandGroup = SequentialCommandGroup(
+            TestAuto(),
+            HomingVision()
+        )
+    }
+
+    override fun isFinished(): Boolean {
+        return false
+    }
+
+    override fun end(interrupted: Boolean) {
+        moveCommandGroup.end(interrupted)
+    }
+}
