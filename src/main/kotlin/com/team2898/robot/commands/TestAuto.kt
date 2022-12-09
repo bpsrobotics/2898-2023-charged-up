367de05381df46ce6a7c97d5229d05ebed076091
Max Leibowitz <ultrabulk0@gmail.com>
2022-12-08 16:25:01 -0800

    Reset initial position properly

--- src/main/kotlin/com/team2898/robot/commands/TestAuto.kt
+++ src/main/kotlin/com/team2898/robot/commands/TestAuto.kt
@@ -6,8 +6,6 @@ import com.team2898.robot.Constants
 import com.pathplanner.lib.PathPlanner
 import com.team2898.robot.subsystems.Drivetrain
 import com.team2898.robot.subsystems.Odometry
-import edu.wpi.first.math.geometry.Pose2d
-import edu.wpi.first.math.geometry.Rotation2d
 import edu.wpi.first.math.trajectory.Trajectory
 import edu.wpi.first.wpilibj2.command.CommandBase
 
@@ -18,20 +16,10 @@ class TestAuto : CommandBase() {
             Constants.DRIVETRAIN_MAX_VELOCITY.metersPerSecondValue(),
             Constants.DRIVETRAIN_MAX_ACCELERATION.metersPerSecondSquaredValue()
     )
-    /*private val path = Drivetrain.trajectoryMaker.builder()
-        .start(Pose2d())
-        .point(1.5, 1.5)
-        .point(0.0, 3.0)
-        .point(-1.5, 1.5)
-        .point(0.0, 0.0)
-        .point(1.5, 1.5)
-        .point(0.0, 3.0)
-        .point(-1.5, 1.5)
-        .end(Pose2d(0.0, 0.0, Rotation2d.fromDegrees(0.0)))
-        .build()*/
 
     override fun initialize() {
-        Odometry.reset(M(0.0), M(0.0), Degrees(0.0))
+        val initial = firstPath.initialPose
+        Odometry.reset(M(initial.x), M(initial.y), Degrees(initial.rotation.degrees))
         Drivetrain.follow(firstPath)
     }
 
