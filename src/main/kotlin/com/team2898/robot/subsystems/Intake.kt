b61dcb344d55ab402e2ca45525be0fd3d24d1cfd
Ozy King <ozypfl@gmail.com>
2022-12-10 14:33:25 -0800

    Changed intake motor to be SparkMax

--- src/main/kotlin/com/team2898/robot/subsystems/Intake.kt
+++ src/main/kotlin/com/team2898/robot/subsystems/Intake.kt
@@ -2,14 +2,17 @@ package com.team2898.robot.subsystems
 
 import com.ctre.phoenix.motorcontrol.TalonSRXControlMode
 import com.ctre.phoenix.motorcontrol.can.TalonSRX
+import com.revrobotics.CANSparkMax
+import com.revrobotics.CANSparkMaxLowLevel
+import com.team2898.robot.RobotMap
 import com.team2898.robot.RobotMap.INTAKE_MOTOR
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 
 object Intake : SubsystemBase() {
-    private val intakeMotor = TalonSRX(INTAKE_MOTOR)
+    private val intakeMotor = CANSparkMax(RobotMap.INTAKE_MOTOR, CANSparkMaxLowLevel.MotorType.kBrushless)
 
     fun setSpeed(power: Double) {
-        intakeMotor.set(TalonSRXControlMode.PercentOutput, power)
+        intakeMotor.set(power)
         if (power > 0.0) {
             Feeder.startIntaking(false)
         }
