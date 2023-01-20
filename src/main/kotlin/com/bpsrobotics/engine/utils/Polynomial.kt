3925ef9a2758de4e72827402aaa3671da169baf7
Ozy King <ozypfl@gmail.com>
2023-01-19 18:11:19 -0800

    Updated polynomial class

--- src/main/kotlin/com/bpsrobotics/engine/utils/Polynomial.kt
+++ src/main/kotlin/com/bpsrobotics/engine/utils/Polynomial.kt
@@ -1,4 +1,25 @@
 package com.bpsrobotics.engine.utils
 
-class Polynomial(vararg coefficients: Double) {
+import kotlin.math.pow
+
+class Polynomial(vararg val coefficients: Double) {
+    /** Degree of the polynomial*/
+    val degree = coefficients.size
+
+    /**Returns the output of a polynomial given X*/
+    operator fun get(x: Double): Double {
+        var returnValue = 0.0;
+        for ((i, k) in coefficients.withIndex()) {
+            returnValue += k * x.pow(i)
+        }
+        return returnValue
+    }
+
+
+//    init {
+//        for(item in coefficients){
+//            this["a"] = item
+//        }
+//    }
+
 }
