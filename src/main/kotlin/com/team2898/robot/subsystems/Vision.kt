341e5350c423f0a0cdbb156468883154c99cbf6e
Ozy King <ozypfl@gmail.com>
2023-01-19 17:34:34 -0800

    Changed ReadMe

--- src/main/kotlin/com/team2898/robot/subsystems/Vision.kt
+++ src/main/kotlin/com/team2898/robot/subsystems/Vision.kt
@@ -1,6 +1,17 @@
 package com.team2898.robot.subsystems
 
+import edu.wpi.first.math.geometry.Pose3d
+import edu.wpi.first.math.geometry.Quaternion
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 
+object Confidence {
+    fun get_confindence(x_dist: Double, y_dist: Double, speed: Double = 0.0) : Double {
+        return 1.0
+    }
+}
+
+
 object Vision : SubsystemBase() {
+    var currentRobotPose = Pose3d()
+
 }
