d8bacbcca52fbffe0ae66d2d268dc2e65659e6d6
Max Leibowitz <ultrabulk0@gmail.com>
2023-01-21 15:43:41 -0800

    for real this time

--- src/test/kotlin/ExampleTest.kt
+++ src/test/kotlin/ExampleTest.kt
@@ -1,2 +1,9 @@
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
 class ExampleTest {
+    @Test
+    fun testReality() {
+        Assertions.assertEquals(4, 2 + 2)
+    }
 }
