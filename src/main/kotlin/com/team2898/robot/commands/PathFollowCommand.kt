5c49e70c987574e9c2497bc37ce9346dcfdd26f9
Max Leibowitz <ultrabulk0@gmail.com>
2023-02-10 16:23:54 -0800

    Added path following command

---
+++ src/main/kotlin/com/team2898/robot/commands/PathFollowCommand.kt
@@ -0,0 +1,36 @@
+package com.team2898.robot.commands
+
+import com.bpsrobotics.engine.utils.`M/s`
+import com.bpsrobotics.engine.utils.MetersPerSecondSquared
+import com.pathplanner.lib.PathPlanner
+import com.team2898.robot.Constants.DRIVETRAIN_MAX_ACCELERATION
+import com.team2898.robot.Constants.DRIVETRAIN_MAX_VELOCITY
+import com.team2898.robot.subsystems.Drivetrain
+import com.team2898.robot.subsystems.Odometry
+import edu.wpi.first.wpilibj.Timer
+import edu.wpi.first.wpilibj2.command.CommandBase
+
+/**
+ * A command that follows a path until it is finished or times out.
+ *
+ * @param filename The filename of the path to load
+ */
+class PathFollowCommand(filename: String, private val resetOdometry: Boolean, maxVel: `M/s` = DRIVETRAIN_MAX_VELOCITY, maxAccel: MetersPerSecondSquared = DRIVETRAIN_MAX_ACCELERATION) : CommandBase() {
+    private val path = PathPlanner.loadPath(filename, maxVel.value, maxAccel.value)
+    private val time = Timer()
+
+    init {
+        time.start()
+    }
+
+    override fun initialize() {
+        if (resetOdometry) {
+            Odometry.reset(path.initialPose)
+        }
+        Drivetrain.follow(path)
+    }
+
+    override fun isFinished(): Boolean {
+        return Drivetrain.mode != Drivetrain.Mode.CLOSED_LOOP || time.hasElapsed(path.totalTimeSeconds * 1.25)
+    }
+}
