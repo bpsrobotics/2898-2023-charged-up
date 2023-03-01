95c74232ea8dded03803dd78f34b398af9ceb38c
Ozy King <ozypfl@gmail.com>
2023-03-01 14:57:29 -0800

    Vision integration changes

--- src/main/kotlin/com/team2898/robot/subsystems/Odometry.kt
+++ src/main/kotlin/com/team2898/robot/subsystems/Odometry.kt
@@ -2,13 +2,20 @@ package com.team2898.robot.subsystems
 
 import com.bpsrobotics.engine.odometry.PoseProvider
 import com.bpsrobotics.engine.utils.*
+import com.team2898.robot.Constants.DRIVETRAIN_TRACK_WIDTH
+import edu.wpi.first.math.Matrix
+import edu.wpi.first.math.Nat
+import edu.wpi.first.math.estimator.DifferentialDrivePoseEstimator
 import edu.wpi.first.math.geometry.Pose2d
 import edu.wpi.first.math.geometry.Rotation2d
+import edu.wpi.first.math.kinematics.DifferentialDriveKinematics
 import edu.wpi.first.math.kinematics.DifferentialDriveOdometry
 import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds
 import edu.wpi.first.util.sendable.SendableBuilder
 import edu.wpi.first.util.sendable.SendableRegistry
+import edu.wpi.first.wpilibj.util.WPILibVersion
 import edu.wpi.first.wpilibj2.command.SubsystemBase
+import kotlin.math.abs
 
 object Odometry : SubsystemBase(), PoseProvider {
 
@@ -16,8 +23,7 @@ object Odometry : SubsystemBase(), PoseProvider {
         val navx = NAVX()
     }
 
-    private val otherProvider = DifferentialDriveOdometry(NavxHolder.navx.rotation2d, 0.0, 0.0)
-
+    private val otherProvider = DifferentialDrivePoseEstimator(DifferentialDriveKinematics(DRIVETRAIN_TRACK_WIDTH.meterValue()), NavxHolder.navx.rotation2d, 0.0, 0.0, Pose2d())
     val leftVel get() =  MetersPerSecond(Drivetrain.leftEncoder.rate)
     val rightVel get() = MetersPerSecond(Drivetrain.rightEncoder.rate)
     val vels get() = DifferentialDriveWheelSpeeds(leftVel.metersPerSecondValue(), rightVel.metersPerSecondValue())
@@ -36,6 +42,11 @@ object Odometry : SubsystemBase(), PoseProvider {
 
     override fun update() {
         pose = otherProvider.update(NavxHolder.navx.rotation2d, Drivetrain.leftEncoder.distance, Drivetrain.rightEncoder.distance)
+        otherProvider.setVisionMeasurementStdDevs(Vision.stdev)
+        otherProvider.addVisionMeasurement(
+                Vision.pose, Vision.lastUpdate //TODO: Get FPGA time of the measurement.
+        )
+
     }
 
     override fun initSendable(builder: SendableBuilder) {
--- src/main/kotlin/com/team2898/robot/subsystems/Vision.kt
+++ src/main/kotlin/com/team2898/robot/subsystems/Vision.kt
@@ -2,40 +2,27 @@ package com.team2898.robot.subsystems
 
 import com.bpsrobotics.engine.utils.Polynomial
 import edu.wpi.first.math.geometry.Pose2d
+import edu.wpi.first.math.geometry.Rotation2d
+import edu.wpi.first.math.geometry.Translation2d
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
 
-object Confidence {
-    //TODO: Place in actual values and find degree of polynomial
-    private const val a = 0.0
-    private const val b = 0.0
-    private const val c = 0.0
-    private const val d = 0.0
 
-    //TODO: Properly set up the appropriate coefficients for each polynomial
-    private val yDistPolynomial = Polynomial(a,b,c,d)
-    private val xDistPolynomial = Polynomial(a,b,c,d)
-    private val speedPolynomial = Polynomial(a,b,c,d)
-
-    operator fun get(x: Double) : Double {
-        return 0.0
-    }
-
-    /** Returns the standard deviation. The higher the value, the less confident we are*/
-    fun getConfindence(x_dist: Double, y_dist: Double, speed: Double = 0.0): Double {
-//        val xDistOutput = xDistPolynomial[x_dist]
-//        val yDistOutput = yDistPolynomial[y_dist]
-//        val speedOutput = speedPolynomial[speed]
-//        return xDistOutput + yDistOutput + speedOutput
-        TODO()
-    }
-}
-
-
+/**
+ * @property x The position of the robot in the field on the X-axis (Long end)
+ * @property y The position of the robot in the field on the Y-axis (Short end)
+ * @property z The height of the robot off of the ground
+ * @property r The rotation, in degrees, of the robot, (Yaw)
+ * @property pose The Pose2d derived from vision measurements.
+ * @property lastUpdate The timestamp of the last update the vision was updated, measured in seconds since epoch
+ */
 object Vision : SubsystemBase() {
     var currentRobotPose = Pose2d()
     val x get() = SmartDashboard.getNumber("VisionZ",0.0)
     val y get() = SmartDashboard.getNumber("VisionX",0.0)
     val z get() = SmartDashboard.getNumber("VisionY",0.0)
-
+    val r get() = SmartDashboard.getNumber("VisionRotation", 0.0)
+    val stdev get() = SmartDashboard.getNumber("VisionSTDEV",1.0) //TODO: Make into Matrix
+    val lastUpdate get() = SmartDashboard.getNumber("VisionLastUpdate", 0.0)
+    val pose get() = Pose2d(Translation2d(x,y), Rotation2d(r))
 }
