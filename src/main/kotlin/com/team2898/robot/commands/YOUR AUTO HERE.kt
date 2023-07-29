6ddab63ee5aed5ad5c62c5231953c12a85e115bc
Ozy King <ozypfl@gmail.com>
2023-07-29 15:13:35 -0700

    Create your own auto!

--- src/main/kotlin/com/team2898/robot/commands/YOUR AUTO HERE.kt
+++ src/main/kotlin/com/team2898/robot/commands/YOUR AUTO HERE.kt
@@ -1,7 +1,18 @@
 package com.team2898.robot.commands
 
+import com.team2898.robot.Constants
+
 class `YOUR AUTO HERE`: CommandBase() {
     override fun initialize() {
+        autoCommandGroup = SequentialCommandGroup(
+            //Move forward
+            //Bring arm up
+            //Outake
+            //Stop outake
+            //Bring arm down (MIDDLECONEGOAL)
+            //Backup
 
+        )
+        autoCommandGroup.schedule()
     }
 }
