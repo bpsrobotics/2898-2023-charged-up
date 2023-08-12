524c06a8787c50bbb0062019baacc028b45da40d
miket <porcupine1555@gmail.com>
2023-08-12 11:40:26 -0700

    work?

--- src/main/kotlin/com/team2898/robot/commands/YOUR AUTO HERE.kt
+++ src/main/kotlin/com/team2898/robot/commands/YOUR AUTO HERE.kt
@@ -1,21 +1,27 @@
 package com.team2898.robot.commands
 
+import com.team2898.robot.Constants
 import edu.wpi.first.wpilibj2.command.CommandBase
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup
+import com.team2898.robot.commands.ArmMove
 
 object `YOUR AUTO HERE`: CommandBase() {
     override fun initialize() {
         val autoCommandGroup = SequentialCommandGroup(
-                //TODO Finish this auto
-            //EX: DriveForward(/* Seconds */ 1.0, /* Speed */ 1.0)
+            DriveForward(/* Seconds */ 1.0, /* Speed */ 0.69),
             // Move forward
+
             // Bring arm up (Constants.ArmHeights.MIDDLECONEGOAL)
+            ArmMove(Constants.ArmHeights.MIDDLECONEGOAL),
             // Outtake
+            ActivateIntake(ActivateIntake.RunningIntakes.RUNOUTTAKE),
             // Wait
+
             // Stop Outtake
             // Bring arm down (MOVING)
+            ArmMove(Constants.ArmHeights.MOVING),
             // Backup
-
+            DriveForward(1.0,-0.69)
         )
         autoCommandGroup.schedule()
     }
